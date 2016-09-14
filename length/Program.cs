24c371a228360557aad29517351051339bf4e7fc
YuriiLepskyi <frostlviv50@gmail.com>
2016-09-14 23:44:00 +0300

    length

---
+++ length/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace length 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             const float pi = 3.141591f;
+             Console.WriteLine("Введiть радiус");
+             float radius = float.Parse(Console.ReadLine());
+             float result = 2*pi*radius;
+
+            Console.WriteLine(result);
+        }
+    }
+}
