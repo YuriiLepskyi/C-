4250a675f950e9a2cb2a76cd9bbd2a8fea6ac794
YuriiLepskyi <frostlviv50@gmail.com>
2016-09-14 23:12:23 +0300

    arithmetik

---
+++ arithmetic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            Console.WriteLine("Write the student's name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("mathematics");
+            float mathematics = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("biology");
+            float biology = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("physics");
+            float physics = float.Parse(Console.ReadLine());
+
+            float arithmetic = (mathematics + biology + physics)/3;
+            Console.WriteLine("arithmetic  " + arithmetic + "  Student  " + name);
+        }
+    }
+}
