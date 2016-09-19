ffb768b6653d2a8b33838904567588ec28295b0c
YuriiLepskyi <frostlviv50@gmail.com>
2016-09-19 22:30:37 +0300

    Polindrom

---
+++ polindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int count;
+            bool isEqual = true;
+            word = Console.ReadLine();
+            count = word.Count ();
+            int lastElementIndex = count - 1;
+            for (int i = 0; i < count / 2; i++) 
+            {
+                if (word[i] != word[lastElementIndex - i])
+                {
+                    isEqual = false;
+                }
+            }
+            if (isEqual)
+            {
+                Console.WriteLine("Word is polindrom:");
+            }
+            else
+            {
+                Console.WriteLine("Word is not polindrom");
+            }
+        }
+    }
+}
