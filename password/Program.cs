b81165c47fa32b711da5b61e27218493cb9c6346
YuriiLepskyi <frostlviv50@gmail.com>
2016-09-21 17:30:44 +0300

    password

---
+++ password/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            do
+            {
+                Console.WriteLine("Please enter password:");
+                password = (Console.ReadLine());
+            }
+            while (password != "rotor");
+            Console.WriteLine("OK");
+        }
+    }
+}
