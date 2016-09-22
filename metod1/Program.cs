c5b3309b121c51a4fe987881a223493dc8dc89a0
YuriiLepskyi <frostlviv50@gmail.com>
2016-09-22 19:08:36 +0300

    metod1

---
+++ metod1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum("String 1", 5, 12, 38);
+        }
+        static void Sum(string a, params int [] number)
+        {
+            int sum = 0;
+            for(int i=0;i<number.Length;i++)
+            {
+                sum += number[i];
+            }    
+            if (sum > 20) 
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("Sorry sum<20");
+            } 
+        }
+    }
+}
