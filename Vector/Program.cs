9e8951010b5cf804cc52435486cc831f1d372f0c
YuriiLepskyi <frostlviv50@gmail.com>
2016-10-13 01:53:00 +0300

    Vector

---
+++ Vector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector[] vectors = new Vector[10];
+            for(int i = 0;i<vectors.Count();i++)
+            {
+                vectors[i] = new Vector();
+                vectors[i].SetVector();
+            }
+            foreach (var v in vectors)
+            {
+                if (v.Lenght > 5) 
+                {
+                    Console.WriteLine(string.Format("X:{0} Y:{1} Sum:{2}", v.X, v.Y, v.Lenght));
+                }else
+                {
+                    Console.WriteLine(string.Format("X:{0} Y:{1}", v.X, v.Y));
+                }
+            }
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Vector
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Lenght { get { return Y - X + 1; } }
+
+        public Vector(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Vector() { }
+        public void SetVector()
+        {
+            int y;
+            Console.WriteLine("Enter X ");
+            X = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter Y ");
+                y = int.Parse(Console.ReadLine());
+            } while (y < X);
+            Y = y;           
+        }
+    }
+}
